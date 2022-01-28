1bdb23635b65d29b56332e988a6b8c8917b7ad52
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-28 12:36:42 +0800

    initial setup

---
+++ src/main/java/io/kanban/board/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.kanban.board.ppmtool.repositories;
+
+import io.kanban.board.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
